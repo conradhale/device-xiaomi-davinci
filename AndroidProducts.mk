004318683313987aabba90a887ed850378e40870
spkal01 <kalligeross@gmail.com>
2020-11-18 07:10:41 +0000

    Aicp-fy + some aicp overlays

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/aicp_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    aicp_davinci-user \
+    aicp_davinci-userdebug \
+    aicp_davinci-eng
