6df8456edd3e3531147cc2f5bcec94ae767333d8
spkal01 <kalligeross@gmail.com>
2021-01-04 13:33:04 +0000

    Include mi cam

--- octavi_davinci.mk
+++ octavi_davinci.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common Octavi stuff.
 $(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
+# Inherit from MiuiCamera vendor
+$(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
+
 PRODUCT_NAME := octavi_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
